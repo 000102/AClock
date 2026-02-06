672e56d30bfd4e07114092a14509891d1a0b28ad
まどかちゃん <89297666+000102@users.noreply.github.com>
2026-02-07 04:58:13 +0800

    Downgrade Kotlin and KSP plugin versions
    Updated Kotlin and KSP plugin versions to 2.1.10.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // 项目级构建配置
 plugins {
     id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
-    id("com.google.devtools.ksp") version "2.3.0-1.0.30" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+    id("com.google.devtools.ksp") version "2.1.10-1.0.29" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.1.10" apply false
 }
