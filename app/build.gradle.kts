ba00f06db4eab2159f799f30f410d64958556822
まどかちゃん <89297666+000102@users.noreply.github.com>
2026-02-07 03:23:48 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     androidTestImplementation(composeBom)
 
     // Core Compose
+    implementation("com.github.skydoves:cloudy:0.5.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
